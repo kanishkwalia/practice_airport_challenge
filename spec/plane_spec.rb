2e19e6ebc232f71720960c54122cb61d50031cb0
Kanishk Walia <kanishk.walia@gmail.com>
2015-04-05 15:15:33 +0100

    Added plane and plane spec files

---
+++ spec/plane_spec.rb
@@ -0,0 +1,25 @@
+require 'plane'
+
+describe Plane do
+	let(:plane) {Plane.new}
+
+	it "has a flying status when created" do
+		expect(plane).to be_flying
+	end
+
+	it "has a flying status when flying" do
+		expect(plane.status).to eq :flying
+	end
+
+	it "can land" do
+		plane.land!
+		expect(plane.status).to eq :landed
+	end
+
+	it "changes its status to flying after taking off" do
+		plane.land!
+		plane.take_off
+		expect(plane.status).to eq :flying
+	end
+
+end
